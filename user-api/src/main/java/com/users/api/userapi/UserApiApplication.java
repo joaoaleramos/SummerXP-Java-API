6daee5cccd835e330de63c4102e98ebab49aef99
Joao Alexandre <aleramosfilho@gmail.com>
2022-03-06 04:21:37 -0300

    fixing routing in gateway

--- user-api/src/main/java/com/users/api/userapi/UserApiApplication.java
+++ user-api/src/main/java/com/users/api/userapi/UserApiApplication.java
@@ -2,14 +2,21 @@ package com.users.api.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 @EnableEurekaClient
 public class UserApiApplication {
 
+
+
+  @LoadBalanced
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiApplication.class, args);
 	}
+
+
 
 }
